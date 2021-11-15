c2c3bfb45eaffb45eed274bf16808f24324be501
tiberiualex <tiberiu.alexander@gmail.com>
2021-11-15 23:14:59 +0200

    More styles, basic homepage setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,15 @@
 import type { NextPage } from "next";
-import styles from "../styles/Home.module.css";
+import { useState } from "react";
+import styles from "../styles/App.module.css";
+
+type UnitSystem = "metric" | "imperial";
 
 const App: NextPage = () => {
+  const [cityInput, setCityInput] = useState<string>("Bucharest");
+  const [triggerFetch, setTriggerFetch] = useState<boolean>(true);
+  const [weatherData, setWeatherData] = useState();
+  const [unitSystem, setUnitSystem] = useState<UnitSystem>("metric");
+
   return (
     <div className={styles.wrapper}>
       <p>Weather App wrapper</p>
