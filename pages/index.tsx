f539512738b50e4e34ba3384731e2abb5b05f573
tiberiualex <tiberiu.alexander@gmail.com>
2021-11-17 21:39:47 +0200

    Add OpenWeather API call

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from "next";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "../styles/App.module.css";
 
 type UnitSystem = "metric" | "imperial";
@@ -9,6 +9,24 @@ const App: NextPage = () => {
   const [triggerFetch, setTriggerFetch] = useState<boolean>(true);
   const [weatherData, setWeatherData] = useState();
   const [unitSystem, setUnitSystem] = useState<UnitSystem>("metric");
+
+  useEffect(() => {
+    const getData = async () => {
+      const res = await fetch("api/data", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ cityInput }),
+      });
+
+      const data = await res.json();
+      setWeatherData({ ...data });
+      setCityInput("");
+    };
+
+    getData();
+  }, [triggerFetch]);
+
+  console.log(weatherData);
 
   return (
     <div className={styles.wrapper}>
